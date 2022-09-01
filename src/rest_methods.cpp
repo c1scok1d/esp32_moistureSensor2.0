758b944cc9f5a9fae6fc9f53a7fffdf52d018126
c1scok1d <rchatman@macinternetservices.com>
2022-09-01 15:52:06 -0500

    turn off debug out put

--- src/rest_methods.cpp
+++ src/rest_methods.cpp
@@ -37,12 +37,12 @@ esp_err_t _http_event_handler_post(esp_http_client_event_t *evt)
        // printf( "HTTP_EVENT_ON_HEADER, key=%s, value=%s\n", evt->header_key, evt->header_value);
         break;
     case HTTP_EVENT_ON_DATA:
-        printf( "HTTP_EVENT_ON_DATA, len=%d\n", evt->data_len);
+        //printf( "HTTP_EVENT_ON_DATA, len=%d\n", evt->data_len);
         /*
          *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
          *  However, event handler can also be used in case chunked encoding is used.
          */
-        if (!esp_http_client_is_chunked_response(evt->client))
+       /* if (!esp_http_client_is_chunked_response(evt->client))
         {
             // If user_data buffer is configured, copy the response into the buffer
             if (evt->user_data)
@@ -64,11 +64,11 @@ esp_err_t _http_event_handler_post(esp_http_client_event_t *evt)
                 memcpy(output_buffer + output_len, evt->data, evt->data_len);
             }
             output_len += evt->data_len;
-        }
+        } */
 
         break;
     case HTTP_EVENT_ON_FINISH:
-        printf( "HTTP_EVENT_ON_FINISH\n");
+        /*printf( "HTTP_EVENT_ON_FINISH\n");
         if (output_buffer != NULL)
         {
             // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
@@ -76,11 +76,11 @@ esp_err_t _http_event_handler_post(esp_http_client_event_t *evt)
             free(output_buffer);
             output_buffer = NULL;
         }
-        output_len = 0;
+        output_len = 0;*/
         break;
     case HTTP_EVENT_DISCONNECTED:
-        printf("HTTP_EVENT_DISCONNECTED\n");
-        int mbedtls_err = 0;
+        //printf("HTTP_EVENT_DISCONNECTED\n");
+        //int mbedtls_err = 0;
         // esp_err_t err = esp_tls_get_and_clear_last_error(NULL,evt->data, &mbedtls_err, NULL);
         // if (err != 0)
         // {
@@ -104,7 +104,7 @@ int POST(String server_uri, String to_send)
 {
     printf(server_uri.c_str());
     printf(to_send.c_str());
-    printf("\nPOST\n");
+    //printf("\nPOST\n");
     char local_response_buffer_post[MAX_HTTP_OUTPUT_BUFFER_POST] = {0};
 
     esp_http_client_handle_t http_client_post; // its being used in the get post and put method files
@@ -134,7 +134,7 @@ int POST(String server_uri, String to_send)
 
     if (err == ESP_OK)
     {
-        printf("Success POST\n");
+        printf("\nSuccess POST\n");
         printf("Status code %d\n", status_code);
         return status_code;
     }
